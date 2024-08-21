ddea07199b3366411410102d8547adf36e09d0f2
oksanulik <kalko_oksanka@ukr.net>
2024-08-21 23:32:05 +0300

    Revert "fin7modfix"
    This reverts commit 74030a5434614b2c2a04b325be99520d10fdbbf4, reversing
    changes made to 6f2ad62a9753e55d5ace31714e72fb45e33ebe31.

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
   (() => {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open]"),
@@ -15,19 +14,3 @@
   })();
 
   
-=======
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-  
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-  })();
->>>>>>> a3697788449a3b5ee3a590f31af2b7cd3080ea7f
